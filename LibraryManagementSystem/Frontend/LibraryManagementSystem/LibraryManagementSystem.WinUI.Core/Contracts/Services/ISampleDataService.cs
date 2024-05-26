2fc5392c4a4d216a1f7e73e671317031ee2458e2
Wong <josuewong797@gmail.com>
2024-05-26 12:07:03 -0600

    Se elimina proyecto WinUI.Core

--- LibraryManagementSystem/Frontend/LibraryManagementSystem/LibraryManagementSystem.WinUI.Core/Contracts/Services/ISampleDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using LibraryManagementSystem.WinUI.Core.Models;
-
-namespace LibraryManagementSystem.WinUI.Core.Contracts.Services;
-
-// Remove this class once your pages/features are using your data.
-public interface ISampleDataService
-{
-    Task<IEnumerable<SampleOrder>> GetGridDataAsync();
-}
