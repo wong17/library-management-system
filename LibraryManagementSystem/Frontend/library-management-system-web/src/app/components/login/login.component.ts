5f10bdb8c4827fbb99643cbd449ef87e6b52e31b
Wong <josuewong797@gmail.com>
2024-05-31 19:48:32 -0600

    Se agregan validaciones a login.component

--- LibraryManagementSystem/Frontend/library-management-system-web/src/app/components/login/login.component.ts
+++ LibraryManagementSystem/Frontend/library-management-system-web/src/app/components/login/login.component.ts
@@ -5,7 +5,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -19,16 +21,26 @@ export class LoginComponent {
   /* Formulario de inicio de sesión */
   loginForm: FormGroup;
 
-  constructor() {
-    this.loginForm = new FormGroup({
-      usernameField: new FormControl(''),
-      passwordField: new FormControl('')
+  constructor(private formBuilder: FormBuilder) {
+    /* Agrupar controles, crear formulario y agregar validaciones */
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(64)]]
     });
   }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.loginForm.value);
+  }
+
+  /* Getters */
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 
 }
