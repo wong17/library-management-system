b14b62f13aed3d24ac132ab137d29c6746050b21
Wong <josuewong797@gmail.com>
2024-06-02 13:41:54 -0600

    Se agregan servicios para todos los schemas en la web

---
+++ LibraryManagementSystem/Frontend/library-management-system-web/src/app/services/security/user/user-endpoint.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserEndpointService {
+
+  /* API url base */
+  private readonly apiUrl: string = environment.apiUrl;
+
+  /* Encabezado http para solicitudes POST y PUT */
+  private readonly httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json, text/plain, */*'
+    })
+  }
+
+  /**
+   * Modulo HttpClient para enviar solicitudes http
+   * @param http
+   */
+  constructor(private readonly http: HttpClient) { }
+
+  
+}
