ac6e69121d76943a6f5c41b88950fb928668d728
Wong <josuewong797@gmail.com>
2024-06-03 13:10:31 -0600

    Se elimina [JsonPropertyName] de ApiResponse

--- LibraryManagementSystem/Backend/LibraryManagementSystem/LibraryManagementSystem.Common/Runtime/ApiResponse.cs
+++ LibraryManagementSystem/Backend/LibraryManagementSystem/LibraryManagementSystem.Common/Runtime/ApiResponse.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using System.Text.Json.Serialization;
 
 namespace LibraryManagementSystem.Common.Runtime
 {
@@ -9,19 +8,15 @@ namespace LibraryManagementSystem.Common.Runtime
          * Resultado de la petición HTTP
          * 0: Éxito, 1: No paso una validación, 2: No existe el recurso, 3: Error en la base de datos
          */
-        [JsonPropertyName("IsSuccess")]
         public int IsSuccess { get; set; } = 1;
 
         /* Código de estado HTTP */
-        [JsonPropertyName("StatusCode")]
         public HttpStatusCode StatusCode { get; set; }
 
         /* Mensaje de exito o error asociado a la petición */
-        [JsonPropertyName("Message")]
         public string? Message { get; set; }
 
         /* Resultado al haber realizado la petición */
-        [JsonPropertyName("Result")]
         public object? Result { get; set; }
     }
 }
