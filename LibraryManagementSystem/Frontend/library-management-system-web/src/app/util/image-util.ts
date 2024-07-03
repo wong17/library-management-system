a64e1664f981b906901fe9bb20acd9fa18b106f8
Wong <josuewong797@gmail.com>
2024-07-03 12:21:26 -0600

    Se agrega utilidad para convertir imagen de base64 a blob

---
+++ LibraryManagementSystem/Frontend/library-management-system-web/src/app/util/image-util.ts
@@ -0,0 +1,29 @@
+export class ImageUtil {
+
+    /**
+     * Convierte una imagen base64 a blob
+     * @param b64Data 
+     * @param contentType 
+     * @param sliceSize 
+     * @returns 
+     */
+    public static b64toBlob(b64Data: string, contentType = '', sliceSize = 512): Blob {
+        const byteCharacters = atob(b64Data);
+        const byteArrays = [];
+
+        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            const byteNumbers = new Array(slice.length);
+            for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            const byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
+        }
+
+        const blob = new Blob(byteArrays, { type: contentType });
+        return blob;
+    }
+} 
