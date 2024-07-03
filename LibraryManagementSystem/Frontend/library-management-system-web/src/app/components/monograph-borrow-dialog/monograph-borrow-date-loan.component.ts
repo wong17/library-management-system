854e92a17f69e8a23e39fce02e71fec84c53ba82
Wong <josuewong797@gmail.com>
2024-07-02 23:10:38 -0600

    Se actualiza componente para agregar fecha de devolucion a las monografias que se soliciten

--- LibraryManagementSystem/Frontend/library-management-system-web/src/app/components/monograph-borrow-dialog/monograph-borrow-date-loan.component.ts
+++ LibraryManagementSystem/Frontend/library-management-system-web/src/app/components/monograph-borrow-dialog/monograph-borrow-date-loan.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
@@ -24,6 +24,8 @@ export class MonographBorrowDateLoanComponent {
 
   updateBorrowedMonographLoanDto: UpdateBorrowedMonographLoanDto | undefined;
   monographLoanDto: MonographLoanDto | undefined;
+  minDate: string | undefined;
+  maxDate: string | undefined;
 
   /**
    * Inicializa el componente
@@ -44,7 +46,7 @@ export class MonographBorrowDateLoanComponent {
   ) {
     /* Agrupar controles, crear formulario y agregar validaciones */
     this.borrowDateForm = this.formBuilder.group({
-      borrowDate: ['', [Validators.required]]
+      borrowDate: ['', [Validators.required, this.dateValidator.bind(this)]]
     });
 
     // Verificar que hayan iniciado sesion
@@ -58,7 +60,7 @@ export class MonographBorrowDateLoanComponent {
       this.closeDialog(true);
     }
 
-    // Si se recibe la información de la solicitud del libro, llenar el dto para hacer la devolución de la solicitud
+    // Si se recibe la información de la solicitud del libro, llenar el dto para hacer el prestamo 
     if (dialogData.data) {
       this.monographLoanDto = dialogData.data as MonographLoanDto;
 
@@ -68,6 +70,46 @@ export class MonographBorrowDateLoanComponent {
         dueDate: new Date() // default
       }
     }
+
+    // Establecer restricciones de fecha y hora según el tipo de préstamo
+    this.setDateRestrictions();
+  }
+
+  /**
+   * Establece restricciones de fecha y hora según el tipo de préstamo
+   */
+  setDateRestrictions() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    
+    const minHour = '07:00';
+    const maxHour = '19:00';
+    
+    this.minDate = `${year}-${month}-${day}T${minHour}`;
+    this.maxDate = `${year}-${month}-${day}T${maxHour}`;
+  }
+
+  /**
+   * Valida que la fecha seleccionada sea válida 
+   * @param control - El control del formulario que contiene la fecha seleccionada
+   * @returns Un objeto con la clave 'invalidDate' si la fecha es inválida, de lo contrario null
+   */
+  dateValidator(control: AbstractControl): { [key: string]: any } | null {
+    const borrowDate = new Date(control.value);
+    const today = new Date();
+    const startHour = 7; // Hora mínima permitida (7 AM)
+    const endHour = 19; // Hora máxima permitida (7 PM)
+    const hour = borrowDate.getHours();
+    const minutes = borrowDate.getMinutes();
+
+    today.setHours(0, 0, 0, 0); // Resetear la hora para asegurar que es hoy
+
+    if (borrowDate.toDateString() !== today.toDateString() || hour < startHour || (hour === endHour && minutes > 0) || hour > endHour) {
+      return { 'invalidDate': true };
+    }
+    return null;
   }
 
   /**
