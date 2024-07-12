c82f1c91d4892ef67f8b5ef4f41c7751260da686
Wong <josuewong797@gmail.com>
2024-07-11 20:47:52 -0600

    Se agrega clase FormFileHelper

---
+++ LibraryManagementSystem/Backend/LibraryManagementSystem/LibraryManagementSystem.Common/Helpers/FormFileHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+
+namespace LibraryManagementSystem.Common.Helpers
+{
+    public static class FormFileHelper
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static byte[]? IFormFileToByteArray(IFormFile? file)
+        {
+            if (file is null)
+                return null;
+
+            using var memoryStream = new MemoryStream();
+            file.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="byteArray"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static IFormFile? ByteArrayToIFormFile(byte[]? byteArray, string fileName = "image.jpg")
+        {
+            if (byteArray is null)
+                return null;
+
+            var stream = new MemoryStream(byteArray);
+            return new FormFile(stream, 0, byteArray.Length, "name", fileName);
+        }
+    }
+}
